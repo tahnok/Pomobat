19a7591da5f9ab39bf658bc452a620e9ec792273
Wesley Ellis <tahnok@gmail.com>
2012-07-24 10:54:57 -0400

    It works! Kinda ugly... but it works!

--- build/pomobat.js
+++ build/pomobat.js
@@ -1,7 +1,8 @@
 (function() {
   var Pomobat,
     __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   Batman.config.minificationErrors = false;
 
@@ -24,6 +25,11 @@
     __extends(PomodorosController, _super);
 
     function PomodorosController() {
+      this.doneBreak = __bind(this.doneBreak, this);
+
+      this.donePomodoro = __bind(this.donePomodoro, this);
+
+      this.updatePomodoro = __bind(this.updatePomodoro, this);
       PomodorosController.__super__.constructor.apply(this, arguments);
       this.set('newPomodoro', new Pomobat.Pomodoro({
         state: "new"
@@ -53,8 +59,66 @@
       var pomodoro;
       pomodoro = this.get('newPomodoro');
       pomodoro.set('state', 'running');
+      pomodoro.set('timeLeft', '0:10');
       pomodoro.save();
-      return console.log(pomodoro.get('title'));
+      return this.startTimer('0:10', this.donePomodoro, this.updatePomodoro);
+    };
+
+    PomodorosController.prototype.updatePomodoro = function(time) {
+      return this.get('newPomodoro').set('timeLeft', time);
+    };
+
+    PomodorosController.prototype.donePomodoro = function() {
+      var pomodoro;
+      pomodoro = this.get('newPomodoro');
+      pomodoro.set('state', 'finished');
+      return alert("Pomodoro done!");
+    };
+
+    PomodorosController.prototype.startBreak = function() {
+      return this.startTimer("0:05", this.doneBreak);
+    };
+
+    PomodorosController.prototype.doneBreak = function() {
+      alert("break's over! get back to work!");
+      return this.set('newPomodoro', new Pomobat.Pomodoro({
+        state: "new"
+      }));
+    };
+
+    PomodorosController.prototype.startTimer = function(time, done, update) {
+      var _this = this;
+      this.set('timeLeft', time);
+      window.tick = function() {
+        return _this.tick(done, update);
+      };
+      return setTimeout(window.tick, 1000);
+    };
+
+    PomodorosController.prototype.tick = function(done, update) {
+      var minutes, seconds, time;
+      time = this.get('timeLeft').split(":");
+      minutes = parseInt(time[0], 10);
+      seconds = parseInt(time[1], 10);
+      if ((minutes === 0) && (seconds === 0)) {
+        return done();
+      } else {
+        if (seconds === 0) {
+          minutes = minutes - 1;
+          seconds = 59;
+        } else {
+          seconds = seconds - 1;
+        }
+        if (seconds < 10) {
+          seconds = "0" + seconds;
+        }
+        time = "" + minutes + ":" + seconds;
+        if (update) {
+          update(time);
+        }
+        this.set('timeLeft', time);
+        return setTimeout(window.tick, 1000);
+      }
     };
 
     return PomodorosController;
@@ -69,7 +133,7 @@
       return Pomodoro.__super__.constructor.apply(this, arguments);
     }
 
-    Pomodoro.encode('title', 'state');
+    Pomodoro.encode('title', 'state', 'timeLeft');
 
     Pomodoro.persist(Batman.LocalStorage);
 
@@ -83,9 +147,21 @@
       }
     });
 
-    Pomodoro.prototype.tester = function() {
-      return true;
-    };
+    Pomodoro.accessor('finished', function() {
+      if (this.get('state') === 'finished') {
+        return true;
+      } else {
+        return false;
+      }
+    });
+
+    Pomodoro.accessor('new', function() {
+      if (this.get('state') === 'new') {
+        return true;
+      } else {
+        return false;
+      }
+    });
 
     return Pomodoro;
 
--- pomobat.coffee
+++ pomobat.coffee
@@ -21,16 +21,64 @@ class Pomobat.PomodorosController extends Batman.Controller
   startPomodoro: ->
     pomodoro = @get('newPomodoro')
     pomodoro.set('state', 'running')
+    pomodoro.set('timeLeft', '0:10')
     pomodoro.save()
-    console.log(pomodoro.get('title'))
+    @startTimer('0:10', @donePomodoro, @updatePomodoro)
+
+  updatePomodoro: (time) =>
+    @get('newPomodoro').set('timeLeft', time)
+
+  donePomodoro: =>
+    pomodoro = @get('newPomodoro')
+    pomodoro.set('state', 'finished')
+    alert("Pomodoro done!")
+
+  startBreak: ->
+    @startTimer("0:05", @doneBreak)
+
+  doneBreak: =>
+    alert("break's over! get back to work!")
+    @set 'newPomodoro', new Pomobat.Pomodoro(state: "new")
+
+  startTimer:(time, done, update) ->
+    @set('timeLeft', time)
+    window.tick = =>
+      @tick(done, update)
+    setTimeout(window.tick, 1000)
+
+  tick:(done, update) ->
+    time = @get('timeLeft').split(":")
+    minutes = parseInt(time[0], 10)
+    seconds = parseInt(time[1], 10)
+    if (minutes == 0) and (seconds == 0)
+      done()
+    else
+      if seconds == 0
+        minutes = minutes - 1
+        seconds = 59
+      else
+        seconds = seconds - 1
+      seconds = "0" + seconds if seconds < 10
+      time = "" + minutes + ":" + seconds
+      update(time) if update
+      @set('timeLeft', time)
+      setTimeout(window.tick, 1000)
+
 
 class Pomobat.Pomodoro extends Batman.Model
-  @encode 'title', 'state'
+  @encode 'title', 'state', 'timeLeft'
   @persist Batman.LocalStorage
   @storageKey: 'pomodoros-batman'
 
   @accessor 'running', ->
     if @get('state') == 'running' then true else false
+
+  @accessor 'finished', ->
+    if @get('state') == 'finished' then true else false
+
+  @accessor 'new', ->
+    if @get('state') == 'new' then true else false
+
 
 
 # Make Pomobat available in the global namespace so it can be used
